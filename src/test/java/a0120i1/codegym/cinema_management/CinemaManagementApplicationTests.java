1cd23cba61ec6858a37f0247f2b6c97f7e67e78f
Your Name <you@example.com>
2022-02-16 19:29:41 +0700

    Create project Spring Cinema-Management

---
+++ src/test/java/a0120i1/codegym/cinema_management/CinemaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package a0120i1.codegym.cinema_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
