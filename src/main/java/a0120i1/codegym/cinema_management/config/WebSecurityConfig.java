b6c31265a33c01714bd3583473a23cc8746d0ccd
nhu <quynhnhuctcn@gmail.com>
2022-02-23 19:42:35 +0700

    update detail-movie-NhuPTQ

--- src/main/java/a0120i1/codegym/cinema_management/config/WebSecurityConfig.java
+++ src/main/java/a0120i1/codegym/cinema_management/config/WebSecurityConfig.java
@@ -44,19 +44,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/api/home").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/login").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/google").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/facebook").permitAll()
-//                .antMatchers("/api/admin").hasRole("ADMIN")
-//                .antMatchers("/api/user").hasRole("USER")
-//                .anyRequest().authenticated()
-//                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .addFilterBefore(this.jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/home").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/google").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/facebook").permitAll()
+                .antMatchers("/api/admin").hasRole("ADMIN")
+                .antMatchers("/api/user").hasRole("USER")
+                .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(this.jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
 }
