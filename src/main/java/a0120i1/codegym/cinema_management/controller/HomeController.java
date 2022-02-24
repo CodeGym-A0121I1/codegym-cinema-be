22265bb6ef5f5d36486df9c1e967d3000c01f3b2
Your Name <you@example.com>
2022-02-24 16:27:45 +0700

    Update try-catch (username not exists)

--- src/main/java/a0120i1/codegym/cinema_management/controller/HomeController.java
+++ src/main/java/a0120i1/codegym/cinema_management/controller/HomeController.java
@@ -22,10 +22,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.social.ExpiredAuthorizationException;
@@ -112,7 +109,11 @@ public class HomeController {
             httpStatus = HttpStatus.BAD_REQUEST;
         } catch (BadCredentialsException badCredentialsException) {
             // Catch username & password exists in database
-            status = "Wrong password or username";
+            status = "Wrong password";
+            httpStatus = HttpStatus.BAD_REQUEST;
+        } catch (InternalAuthenticationServiceException internalAuthenticationServiceException) {
+            System.out.println(internalAuthenticationServiceException.toString());
+            status = "Username not exists";
             httpStatus = HttpStatus.BAD_REQUEST;
         } catch (Exception exception) {
             status = "Error server";
