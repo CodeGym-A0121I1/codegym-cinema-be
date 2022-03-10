8b15c485eaa2eaa3455ed2292b963e018e7210dc
nhu <quynhnhuctcn@gmail.com>
2022-03-11 00:51:44 +0700

    update detail-movie-NhuPTQ

--- src/main/java/a0120i1/codegym/cinema_management/repository/IShowTimeRepository.java
+++ src/main/java/a0120i1/codegym/cinema_management/repository/IShowTimeRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface IShowTimeRepository extends JpaRepository<ShowTime,String> {
     List<ShowTime> findShowTimeByMovie_Id(String idMovie);
+
+   List<ShowTime> findShowTimeByMovie_IdAndTheater_Id(String idMovie, String idTheater);
 }
