8d8ceabb3f9ab47c9b3f02d54b20652544f09035
Uyen Nguyen <nguyenuyen552000@gmail.com>
2022-03-13 22:31:25 +0700

    update get mappping showtime

--- src/main/java/a0120i1/codegym/cinema_management/repository/IShowTimeRepository.java
+++ src/main/java/a0120i1/codegym/cinema_management/repository/IShowTimeRepository.java
@@ -4,9 +4,11 @@ import a0120i1.codegym.cinema_management.model.booking.ShowTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface IShowTimeRepository extends JpaRepository<ShowTime,String> {
     List<ShowTime> findShowTimeByMovie_Id(String idMovie);
+    List<ShowTime> findShowTimeByStartDate(LocalDate date);
 }
